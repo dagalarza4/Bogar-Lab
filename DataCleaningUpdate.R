82a3430285e2affab7fee90174170cb40c8e2480
Demorie Galarza <dagalarza@ucdavis.edu>
2024-07-21 15:06:15 -0700

    New csv files, attempt to include info didn't work

--- DataCleaningUpdate.R
+++ DataCleaningUpdate.R
@@ -117,7 +117,7 @@ testplot <- ggplot(data = alltogether) +
 
 print(testplot)
 
-#New boxplot changes
+#New boxplot changes 07/20/23
 alltogether <- alltogether %>%
   mutate(Species = case_when(
     grepl("NM", Species_Treatment) ~ "NM",
@@ -144,7 +144,11 @@ testplot <- ggplot(data = alltogether) +
 print(testplot)
 
 
-##Atempt at finding water loss/hr in positive values
+
+### Graph below is best ###
+
+##Attempt at finding water loss/hr in positive values 
+#07/21/23
 
 hourly_loss <- last_and_third_to_last %>%
   arrange(ID, desc(newtime)) %>%
@@ -177,6 +181,86 @@ alltogether <- left_join(hourly_loss, treatments)
 alltogether$Species <- factor(alltogether$Species,
                               levels = c("NM", "RP", "SP", "TC", "R+S", "S+T"))
 
+testplot <- ggplot(data = alltogether) +
+  theme_classic() +
+  theme(axis.text.x = element_text(face = "italic")) +
+  geom_boxplot(aes(x = Species, y = mass_per_hr, color = Treatment), outlier.shape = NA) +
+  geom_jitter(aes(x = Species, y = mass_per_hr, color = Treatment)) +
+  xlab("Fungal Species") + 
+  ylab("Transpiration Rate (water loss g/hr)") +
+  scale_color_manual(name = "Treatment", values = c("control" = "deepskyblue", "drought" = "red")) +
+  labs(color = "Treatment") +
+  geom_hline(yintercept = c(0.05, 0.1, 0.15, 0.2, 0.25), color = "grey", linetype = "solid") +
+  ylim(0, 0.25)
+
+print(testplot)
+### Above graph is best
+
+
+
+
+
+#New attempt with Lysimetry+Info.csv- did not work
+library(readxl)
+
+lys_info <- "Lysimetry+Info.xlsx"
+
+#Convert each sheet into a data frame
+harvest_days <- read_excel(lys_info, sheet = "Harvest Days")
+days_watered <- read_excel(lys_info, sheet = "Days Watered")
+
+write.csv(harvest_days, "Harvest_Days.csv", row.names = FALSE)
+write.csv(days_watered, "Days_Watered.csv", row.names = FALSE)
+
+require(tidyverse)
+require(readxl)
+
+lysimetry_data <- read_csv("Lysimetry_data.csv")
+treatments <- read_csv("Treatment_key.csv") %>%
+  mutate(ID = Plant_ID)
+harvest_days <- read_excel("Lysimetry+Info.xlsx", sheet = "Harvest Days") %>%
+  rename(ID = Sample, HarvestDate = HarvestDate)
+
+# Reshape the lysimetry data
+reshaped_data <- lysimetry_data %>%
+  pivot_longer(cols = -ID,
+               names_to = c(".value", "date"),
+               names_pattern = "(sample|Time)(.*)") %>%
+  drop_na() %>%
+  mutate(newtime = parse_date_time(Time, "mdy_HM"))
+
+for (i in 1:nrow(reshaped_data)) {
+  if (is.na(reshaped_data$newtime[i])) {
+    reshaped_data$newtime[i] = parse_date_time(reshaped_data$Time[i], "mdy_HMS")
+  }
+}
+
+# Merge with harvest days
+reshaped_data <- left_join(reshaped_data, harvest_days, by = "ID")
+
+# Filter data for the last 2 days before harvest
+last_two_days_before_harvest <- reshaped_data %>%
+  group_by(ID) %>%
+  filter(difftime(HarvestDate, newtime, units = "days") <= 2) %>%
+  arrange(ID, desc(newtime)) %>%
+  slice_head(n = 2) %>%
+  ungroup()
+
+# Calculate hourly loss
+hourly_loss <- last_two_days_before_harvest %>%
+  group_by(ID) %>%
+  summarize(massdiff_g = last(sample) - first(sample), # Corrected calculation
+            timediff = as.numeric(difftime(first(newtime), last(newtime), units = "hours"))) %>%
+  mutate(mass_per_hr = massdiff_g / timediff) # Directly calculate mass per hour
+
+# Combine with treatments data
+alltogether <- left_join(hourly_loss, treatments)
+
+# Update species factor levels
+alltogether$Species <- factor(alltogether$Species,
+                              levels = c("NM", "RP", "SP", "TC", "R+S", "S+T"))
+
+# Plotting
 testplot <- ggplot(data = alltogether) +
   theme_classic() +
   theme(axis.text.x = element_text(face = "italic")) +
