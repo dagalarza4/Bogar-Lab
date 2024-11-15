a09b61fab6ee83a4422ca849d004e80c23513530
Demorie Galarza <dagalarza@ucdavis.edu>
2024-11-15 11:42:01 -0800

    Added columns for time difference

--- DFD Data.R
+++ DFD Data.R
@@ -403,3 +403,362 @@ ggplot(subset(Harvest_Data_Clean, Species != "NM" & perccol != 0), aes(x = percc
 
 
 
+
+
+#### Attempt at Time Differences- Lysimetry stuff ####
+library(readxl)
+Lysimetry_Updated <- read_excel("Lysimetry_Updated.xlsx")
+Lysimetry_Super_Update <- Lysimetry_Updated
+watering_data <- read_excel("Lysimetry+Info.xlsx", sheet = "Days Watered")
+
+#Convert to long format
+library(tidyr)
+watering_data_long <- watering_data %>%
+  pivot_longer(cols = -ID, names_to = "Date", values_to = "WaterAdded") %>%
+  mutate(Date = as.Date(Date, format = "%m/%d/%Y"))
+
+Lysimetry_Super_Update_long <- Lysimetry_Super_Update %>%
+  pivot_longer(
+    cols = starts_with("Time"),  # Select columns starting with 'Time'
+    names_to = "Date",           # Create a new 'Date' column from the time column names
+    values_to = "Time"           # Corresponding time values
+  ) %>%
+  mutate(Date = str_extract(Date, "\\d{2}_\\d{2}_\\d{2}"),   # Extract the date part (e.g., 06_19_23)
+         Date = as.Date(Date, format = "%m_%d_%y"))          # Convert to Date type
+
+
+
+
+
+
+
+#Merge Watering Information with Lysimetry Data
+Lysimetry_Super_Update <- Lysimetry_Super_Update %>%
+  mutate(Date = as.Date(Date)) %>%
+  left_join(watering_data_long, by = c("ID", "Date"))
+
+
+
+
+
+
+# Convert the date/time columns to POSIXct format
+Lysimetry_Super_Update$Time06_19_23 <- as.POSIXct(Lysimetry_Super_Update$`Time06/19/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time06_21_23 <- as.POSIXct(Lysimetry_Super_Update$`Time06/21/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time06_22_23 <- as.POSIXct(Lysimetry_Super_Update$`Time06/22/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time06_23_23 <- as.POSIXct(Lysimetry_Super_Update$`Time06/23/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time06_26_23 <- as.POSIXct(Lysimetry_Super_Update$`Time06/26/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time06_27_23 <- as.POSIXct(Lysimetry_Super_Update$`Time06/27/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time06_28_23 <- as.POSIXct(Lysimetry_Super_Update$`Time06/28/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time06_29_23 <- as.POSIXct(Lysimetry_Super_Update$`Time06/29/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_03_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/03/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_05_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/05/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_06_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/06/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_07_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/07/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_10_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/10/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_11_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/11/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_12_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/12/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_13_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/13/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_17_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/17/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_18_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/18/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_19_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/19/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_20_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/20/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_24_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/24/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_25_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/25/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_26_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/26/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_27_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/27/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time07_31_23 <- as.POSIXct(Lysimetry_Super_Update$`Time07/31/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time08_01_23 <- as.POSIXct(Lysimetry_Super_Update$`Time08/01/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time08_02_23 <- as.POSIXct(Lysimetry_Super_Update$`Time08/02/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time08_03_23 <- as.POSIXct(Lysimetry_Super_Update$`Time08/03/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time08_07_23 <- as.POSIXct(Lysimetry_Super_Update$`Time08/07/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time08_08_23 <- as.POSIXct(Lysimetry_Super_Update$`Time08/08/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time08_09_23 <- as.POSIXct(Lysimetry_Super_Update$`Time08/09/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time08_10_23 <- as.POSIXct(Lysimetry_Super_Update$`Time08/10/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time08_14_23 <- as.POSIXct(Lysimetry_Super_Update$`Time08/14/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time08_15_23 <- as.POSIXct(Lysimetry_Super_Update$`Time08/15/23`, format="%m/%d/%y %H:%M")
+
+Lysimetry_Super_Update$Time08_16_23 <- as.POSIXct(Lysimetry_Super_Update$`Time08/16/23`, format="%m/%d/%y %H:%M")
+
+
+# List of time columns in order
+time_columns <- c("Time06_19_23", "Time06_21_23", "Time06_22_23", "Time06_23_23", "Time06_26_23", 
+                  "Time06_27_23", "Time06_28_23", "Time06_29_23", "Time07_03_23", "Time07_05_23", 
+                  "Time07_06_23", "Time07_07_23", "Time07_10_23", "Time07_11_23", "Time07_12_23", 
+                  "Time07_13_23", "Time07_17_23", "Time07_18_23", "Time07_19_23", "Time07_20_23", 
+                  "Time07_24_23", "Time07_25_23", "Time07_26_23", "Time07_27_23", "Time07_31_23", 
+                  "Time08_01_23", "Time08_02_23", "Time08_03_23", "Time08_07_23", "Time08_08_23", 
+                  "Time08_09_23", "Time08_10_23", "Time08_14_23", "Time08_15_23", "Time08_16_23")
+
+
+
+# List of date columns in the format "TimeMM/DD/YY"
+time_columns <- grep("Time", colnames(Lysimetry_Super_Update), value = TRUE)
+
+#Create columns for time difference
+Lysimetry_Super_Update$Time_diff_06_19_06_21 <- 
+  Lysimetry_Super_Update$Time06_21_23 - Lysimetry_Super_Update$Time06_19_23
+
+Lysimetry_Super_Update$Time_diff_06_21_06_22 <- 
+  Lysimetry_Super_Update$Time06_22_23 - Lysimetry_Super_Update$Time06_21_23
+
+Lysimetry_Super_Update$Time_diff_06_22_06_23 <- 
+  Lysimetry_Super_Update$Time06_23_23 - Lysimetry_Super_Update$Time06_22_23
+
+Lysimetry_Super_Update$Time_diff_06_23_06_26 <- 
+  Lysimetry_Super_Update$Time06_26_23 - Lysimetry_Super_Update$Time06_23_23
+
+Lysimetry_Super_Update$Time_diff_06_26_06_27 <- 
+  Lysimetry_Super_Update$Time06_27_23 - Lysimetry_Super_Update$Time06_26_23
+
+Lysimetry_Super_Update$Time_diff_06_27_06_28 <- 
+  Lysimetry_Super_Update$Time06_28_23 - Lysimetry_Super_Update$Time06_27_23
+
+Lysimetry_Super_Update$Time_diff_06_28_06_29 <- 
+  Lysimetry_Super_Update$Time06_29_23 - Lysimetry_Super_Update$Time06_28_23
+
+Lysimetry_Super_Update$Time_diff_06_29_07_03 <- 
+  Lysimetry_Super_Update$Time07_03_23 - Lysimetry_Super_Update$Time06_29_23
+
+Lysimetry_Super_Update$Time_diff_07_03_07_05 <- 
+  Lysimetry_Super_Update$Time07_05_23 - Lysimetry_Super_Update$Time07_03_23
+
+Lysimetry_Super_Update$Time_diff_07_05_07_06 <- 
+  Lysimetry_Super_Update$Time07_06_23 - Lysimetry_Super_Update$Time07_05_23
+
+Lysimetry_Super_Update$Time_diff_07_06_07_07 <- 
+  Lysimetry_Super_Update$Time07_07_23 - Lysimetry_Super_Update$Time07_06_23
+
+Lysimetry_Super_Update$Time_diff_07_07_07_10 <- 
+  Lysimetry_Super_Update$Time07_10_23 - Lysimetry_Super_Update$Time07_07_23
+
+Lysimetry_Super_Update$Time_diff_07_10_07_11 <- 
+  Lysimetry_Super_Update$Time07_11_23 - Lysimetry_Super_Update$Time07_10_23
+
+Lysimetry_Super_Update$Time_diff_07_11_07_12 <- 
+  Lysimetry_Super_Update$Time07_12_23 - Lysimetry_Super_Update$Time07_11_23
+
+Lysimetry_Super_Update$Time_diff_07_11_07_13 <- 
+  Lysimetry_Super_Update$Time07_13_23 - Lysimetry_Super_Update$Time07_11_23
+
+Lysimetry_Super_Update$Time_diff_07_13_07_17 <- 
+  Lysimetry_Super_Update$Time07_17_23 - Lysimetry_Super_Update$Time07_13_23
+
+Lysimetry_Super_Update$Time_diff_07_17_07_18 <- 
+  Lysimetry_Super_Update$Time07_18_23 - Lysimetry_Super_Update$Time07_17_23
+
+Lysimetry_Super_Update$Time_diff_07_18_07_19 <- 
+  Lysimetry_Super_Update$Time07_19_23 - Lysimetry_Super_Update$Time07_18_23
+
+Lysimetry_Super_Update$Time_diff_07_18_07_20 <- 
+  Lysimetry_Super_Update$Time07_20_23 - Lysimetry_Super_Update$Time07_18_23
+
+Lysimetry_Super_Update$Time_diff_07_20_07_24 <- 
+  Lysimetry_Super_Update$Time07_24_23 - Lysimetry_Super_Update$Time07_20_23
+
+Lysimetry_Super_Update$Time_diff_07_24_07_25 <- 
+  Lysimetry_Super_Update$Time07_25_23 - Lysimetry_Super_Update$Time07_24_23
+
+Lysimetry_Super_Update$Time_diff_07_25_07_26 <- 
+  Lysimetry_Super_Update$Time07_26_23 - Lysimetry_Super_Update$Time07_25_23
+
+Lysimetry_Super_Update$Time_diff_07_24_07_27 <- 
+  Lysimetry_Super_Update$Time07_27_23 - Lysimetry_Super_Update$Time07_24_23
+
+Lysimetry_Super_Update$Time_diff_07_27_07_31 <- 
+  Lysimetry_Super_Update$Time07_31_23 - Lysimetry_Super_Update$Time07_27_23
+
+Lysimetry_Super_Update$Time_diff_07_31_08_01 <- 
+  Lysimetry_Super_Update$Time08_01_23 - Lysimetry_Super_Update$Time07_31_23
+
+Lysimetry_Super_Update$Time_diff_08_01_08_02 <- 
+  Lysimetry_Super_Update$Time08_02_23 - Lysimetry_Super_Update$Time08_01_23
+
+Lysimetry_Super_Update$Time_diff_08_01_08_03 <- 
+  Lysimetry_Super_Update$Time08_03_23 - Lysimetry_Super_Update$Time08_01_23
+
+Lysimetry_Super_Update$Time_diff_08_03_08_07 <- 
+  Lysimetry_Super_Update$Time08_07_23 - Lysimetry_Super_Update$Time08_03_23
+
+Lysimetry_Super_Update$Time_diff_08_07_08_08 <- 
+  Lysimetry_Super_Update$Time08_08_23 - Lysimetry_Super_Update$Time08_07_23
+
+Lysimetry_Super_Update$Time_diff_08_08_08_09 <- 
+  Lysimetry_Super_Update$Time08_09_23 - Lysimetry_Super_Update$Time08_08_23
+
+Lysimetry_Super_Update$Time_diff_08_07_08_10 <- 
+  Lysimetry_Super_Update$Time08_10_23 - Lysimetry_Super_Update$Time08_07_23
+
+Lysimetry_Super_Update$Time_diff_08_10_08_14 <- 
+  Lysimetry_Super_Update$Time08_14_23 - Lysimetry_Super_Update$Time08_10_23
+
+Lysimetry_Super_Update$Time_diff_08_14_08_15 <- 
+  Lysimetry_Super_Update$Time08_15_23 - Lysimetry_Super_Update$Time08_14_23
+
+Lysimetry_Super_Update$Time_diff_08_15_08_16 <- 
+  Lysimetry_Super_Update$Time08_16_23 - Lysimetry_Super_Update$Time08_15_23
+
+# Convert difftime to numeric in hours
+Lysimetry_Super_Update$Time_diff_06_19_06_21 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_06_19_06_21, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_06_21_06_22 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_06_21_06_22, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_06_22_06_23 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_06_22_06_23, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_06_23_06_26 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_06_23_06_26, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_06_26_06_27 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_06_26_06_27, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_06_27_06_28 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_06_27_06_28, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_06_28_06_29 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_06_28_06_29, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_06_29_07_03 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_06_29_07_03, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_03_07_05 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_03_07_05, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_05_07_06 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_05_07_06, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_06_07_07 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_06_07_07, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_07_07_10 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_07_07_10, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_10_07_11 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_10_07_11, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_11_07_12 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_11_07_12, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_11_07_13 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_11_07_13, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_13_07_17 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_13_07_17, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_17_07_18 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_17_07_18, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_18_07_19 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_18_07_19, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_18_07_20 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_18_07_20, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_20_07_24 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_20_07_24, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_24_07_25 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_24_07_25, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_25_07_26 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_25_07_26, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_24_07_27 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_24_07_27, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_27_07_31 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_27_07_31, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_07_31_08_01 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_07_31_08_01, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_08_01_08_02 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_08_01_08_02, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_08_01_08_03 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_08_01_08_03, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_08_03_08_07 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_08_03_08_07, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_08_07_08_08 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_08_07_08_08, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_08_08_08_09 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_08_08_08_09, 
+  units = "hours"
+)
+Lysimetry_Super_Update$Time_diff_08_07_08_10 <- as.numeric(
+  Lysimetry_Super_Update$Time_diff_08_09_08_10, 
+  units = "hours"
+)
+#Paused on 08/07 and 08/10 since watering info needs to be added
+
+
+summary(Lysimetry_Super_Update$Time_diff_08_07_08_10)
+
+
